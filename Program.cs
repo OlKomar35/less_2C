75fb1c1178db1e95fdc344255ba05a2e198dff42
Olga Komar <komar_ol@mail.ru>
2023-04-08 18:57:17 +0300

    added solution to task 2

--- Program.cs
+++ Program.cs
@@ -4,3 +4,24 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
+Console.Write("Введите a=");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b=");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите c=");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+
+Console.WriteLine($"max = {max}");
